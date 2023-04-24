06526229e5ee56294bb75f4c6f3b32f8567584ce
ExticainGeek <122371798+ExticainGeek@users.noreply.github.com>
2023-04-24 15:05:52 +0530

    Add files via upload

---
+++ CAN_read.ino
@@ -0,0 +1,37 @@
+#include <LiquidCrystal_PCF8574.h>
+
+#include <SPI.h>
+#include <mcp2515.h>
+#include <Wire.h>
+#include <LiquidCrystal_PCF8574.h>
+
+struct can_frame canMsg;
+MCP2515 mcp2515(10);
+
+
+void setup() {
+  Serial.begin(115200);
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
+  
+  Serial.println("------- CAN Read ----------");
+  Serial.println("ID  DLC   DATA");
+}
+
+void loop() {
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    Serial.print(canMsg.can_id, HEX); // print ID
+    Serial.print(" "); 
+    Serial.print(canMsg.can_dlc, HEX); // print DLC
+    Serial.print(" ");
+    
+    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+      Serial.print(canMsg.data[i],HEX);
+      Serial.print(" ");
+    }
+
+    Serial.println();      
+  }
+}
